fa376fcd18ac63b0d4a663a766476bd00ab38ffa
CHOIAHJIN <chldkwls6532@naver.com>
2024-07-02 16:15:34 +0900

    Python 활용 데이터 분석
    Python 활용 데이터 분석

---
+++ list.py
@@ -0,0 +1,29 @@
+choiahjin = ["최아진", '24', '010-8790-****']
+name = choiahjin[0]
+age = choiahjin[1]
+phone = choiahjin[2]
+print(type(choiahjin))
+
+print(type(choiahjin))
+print(choiahjin[-1])
+
+print(name, age, phone)
+
+names = [['강수경', '강혜나', '김민석'], ['20', '21', '22'], ['010-0000-0000', '010-0000-0001', '010-0000-0002']]
+print(names[0][0:2])
+
+numbers = [1, 2, 3, 4, 5]
+result = numbers[2]+numbers[-1]
+print(result)
+
+print(len(numbers))
+print(len(names[0]))
+
+# 리스트에 요소 추가하기
+last = [1,2,3]
+last.append(30)
+print(last)
+last.remove(3)
+print(last)
+print(type(last))
+
